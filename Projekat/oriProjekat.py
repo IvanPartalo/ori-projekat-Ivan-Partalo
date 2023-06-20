0f2a20dd0cf55ecc7da23d2cc00de6d254f21fef
IvanPartalo <ivanpartalo123@gmail.com>
2023-06-20 17:03:37 +0200

    Added target boxes and target surface

--- Projekat/oriProjekat.py
+++ Projekat/oriProjekat.py
@@ -54,35 +54,104 @@ class Robot(pygame.sprite.Sprite):
         self.pos.y += self.direction.y * self.speed
         self.rect.y = round(self.pos.y)
 
-class warehouse():
+
+class Box(pygame.sprite.Sprite):
+    def __init__(self, groups, x, y):
+        super().__init__(groups)
+        self.image = pygame.Surface((10, 10))
+        self.image.fill('blue')
+        self.rect = self.image.get_rect(topleft=(x, y))
+        self.pos = pygame.math.Vector2(self.rect.center)
+        self.attached_right = False
+        self.attached_left = False
+        self.attached_bottom = False
+        self.attached_up = False
+        self.attached = 0
+        self.difference = 0
+
+    def check_if_robot_attached(self, x, y):
+        if y - 13 <= self.rect.bottom <= y - 7 and self.rect.center[0] - 10 <= x <= self.rect.center[0] + 10:
+            self.difference = self.rect.center[0] - x
+            self.attached = 1
+            self.attached_bottom = True
+        if self.rect.center[1] - 10 <= y <= self.rect.center[1] + 10 and x + 7 <= self.rect.left <= x + 13:
+            self.difference = self.rect.center[1] - y
+            self.attached = 1
+            self.attached_left = True
+        if y + 7 <= self.rect.top <= y + 13 and self.rect.center[0] - 10 <= x <= self.rect.center[0] + 10:
+            self.difference = self.rect.center[0] - x
+            self.attached = 1
+            self.attached_up = True
+        if self.rect.center[1] - 10 <= y <= self.rect.center[1] + 10 and x - 13 <= self.rect.right <= x - 7:
+            self.difference = self.rect.center[1] - y
+            self.attached = 1
+            self.attached_right = True
+
+    def move_with_robot(self, x, y):
+        # cuva se pozicija gde je bila kutija kada ju je robot prikljucio
+        # posto se salje centar robota, sabira/oduzima se sa 5 
+        if self.attached_bottom:
+            self.rect.right = self.difference + x + 5
+            self.rect.bottom = y - 5
+            self.pos.x = self.rect.x
+            self.pos.y = self.rect.y
+        if self.attached_left:
+            self.rect.top = self.difference + y - 5
+            self.rect.left = x + 5
+            self.pos.x = self.rect.x
+            self.pos.y = self.rect.y
+        if self.attached_up:
+            self.rect.right = self.difference + x + 5
+            self.rect.top = y + 5
+            self.pos.x = self.rect.x
+            self.pos.y = self.rect.y
+        if self.attached_right:
+            self.rect.top = self.difference + y - 5
+            self.rect.right = x - 5
+            self.pos.x = self.rect.x
+            self.pos.y = self.rect.y
+
+    def update(self, dir):
+        pass
+
+class Warehouse():
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((200,200), pygame.SCALED)
         self.all_sprites = pygame.sprite.Group()
         self.collision_sprites = pygame.sprite.Group()
         self.robot = Robot([self.all_sprites, self.collision_sprites])
+        self.box1 = Box(self.all_sprites, 45, 40)
+        self.box2 = Box(self.all_sprites, 45, 60)
+        self.boxes=[]
+        self.boxes.append(self.box1)
+        self.boxes.append(self.box2)
         self.done = False
         self.reward = 0
 
     def reset(self):
         self.robot.rect = self.robot.image.get_rect(topleft = (90, 130))
         self.robot.pos = pygame.math.Vector2(self.robot.rect.topleft)
-        
         self.done = False
         self.reward = 0
             
     def step(self, dir):
         self.screen.fill('white')
+        pygame.draw.rect(self.screen, (231, 255, 182), pygame.Rect(60, 60, 60, 60))
         self.all_sprites.update(dir)
         self.all_sprites.draw(self.screen)
-        
+        for box in self.boxes:
+            if box.attached:
+                box.move_with_robot(self.robot.rect.center[0], self.robot.rect.center[1])
+            else:
+                box.check_if_robot_attached(self.robot.rect.center[0], self.robot.rect.center[1])
         if self.robot.collided_with_window():
             self.done = True
         pygame.display.update()
         return self.done
     
 pygame.init()
-env = warehouse()
+env = Warehouse()
 done = env.step(3)
 while not done:
     pygame.event.get()
