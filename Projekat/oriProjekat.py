e4a2247b76f0a665081b596605ccd68fb4f84b25
IvanPartalo <ivanpartalo123@gmail.com>
2023-06-23 14:14:58 +0200

    Preparing environment for training

--- Projekat/oriProjekat.py
+++ Projekat/oriProjekat.py
@@ -62,6 +62,7 @@ class Box(pygame.sprite.Sprite):
         self.image.fill('blue')
         self.rect = self.image.get_rect(topleft=(x, y))
         self.pos = pygame.math.Vector2(self.rect.center)
+        self.old_rect = self.rect.copy()
         self.attached_right = False
         self.attached_left = False
         self.attached_bottom = False
@@ -87,6 +88,8 @@ class Box(pygame.sprite.Sprite):
             self.attached = 1
             self.attached_right = True
 
+    
+
     def move_with_robot(self, x, y):
         # cuva se pozicija gde je bila kutija kada ju je robot prikljucio
         # posto se salje centar robota, sabira/oduzima se sa 5 
@@ -111,8 +114,30 @@ class Box(pygame.sprite.Sprite):
             self.pos.x = self.rect.x
             self.pos.y = self.rect.y
 
+    def is_closer_to_target(self):
+        old_dist = 0
+        dist = 0
+        # znaci umesto ovako zakucanih vrednosti bolje je da promenim na neke "konstante" koje cu menjati
+        old_dist = abs(self.old_rect.center[0] - 90) + abs(self.old_rect.center[1] - 90)
+        dist = abs(self.rect.center[0] - 90) + abs(self.rect.center[1] - 90)
+        if dist < old_dist:
+            return True
+        else:
+            return False
+
+    def is_further_from_target(self):
+        old_dist = 0
+        dist = 0
+        # znaci umesto ovako zakucanih vrednosti bolje je da promenim na neke "konstante" koje cu menjati
+        old_dist = abs(self.old_rect.center[0] - 90) + abs(self.old_rect.center[1] - 90)
+        dist = abs(self.rect.center[0] - 90) + abs(self.rect.center[1] - 90)
+        if dist > old_dist:
+            return True
+        else:
+            return False
+
     def update(self, dir):
-        pass
+        self.old_rect = self.rect.copy()
 
 class Warehouse():
     def __init__(self):
@@ -128,13 +153,43 @@ class Warehouse():
         self.boxes.append(self.box2)
         self.done = False
         self.reward = 0
+        self.state = [0, 0, 0, 0, 0, 0, 0]
 
     def reset(self):
         self.robot.rect = self.robot.image.get_rect(topleft = (90, 130))
         self.robot.pos = pygame.math.Vector2(self.robot.rect.topleft)
         self.done = False
         self.reward = 0
-            
+        self.state = [self.normalize(self.robot.rect.center[0]), self.normalize(self.robot.rect.center[1]),
+                      self.normalize(self.box1.rect.center[0]), self.normalize(self.box1.rect.center[1]),
+                      self.normalize(90), self.normalize(90),
+                      self.box1.attached]
+
+    def normalize(self, x):
+        return (2 * x / 200) - 1
+
+    def is_robot_closer_to_box(self, box):
+        old_dist = 0
+        dist = 0
+        old_dist = abs(self.robot.old_rect.center[0] - box.old_rect.center[0]) + abs(
+            self.robot.old_rect.center[1] - box.old_rect.center[1])
+        dist = abs(self.robot.rect.center[0] - box.rect.center[0]) + abs(
+            self.robot.rect.center[1] - box.rect.center[1])
+        if dist < old_dist:
+            return True
+        else:
+            return False
+
+    def update_reward(self):
+        if self.box1.is_closer_to_target():
+            self.reward = 0.35
+        elif self.box1.is_further_from_target():
+            self.reward = -0.125
+        elif self.is_robot_closer_to_box(self.box1):
+            self.reward = 0.215
+        else:
+            self.reward = -0.025
+       
     def step(self, dir):
         self.screen.fill('white')
         pygame.draw.rect(self.screen, (231, 255, 182), pygame.Rect(60, 60, 60, 60))
@@ -145,9 +200,14 @@ class Warehouse():
                 box.move_with_robot(self.robot.rect.center[0], self.robot.rect.center[1])
             else:
                 box.check_if_robot_attached(self.robot.rect.center[0], self.robot.rect.center[1])
+        self.update_reward()
         if self.robot.collided_with_window():
             self.done = True
         pygame.display.update()
+        self.state = [self.normalize(self.robot.rect.center[0]), self.normalize(self.robot.rect.center[1]),
+                      self.normalize(self.box1.rect.center[0]), self.normalize(self.box1.rect.center[1]),
+                      self.normalize(90), self.normalize(90),
+                      self.box1.attached]
         return self.done
     
 pygame.init()
